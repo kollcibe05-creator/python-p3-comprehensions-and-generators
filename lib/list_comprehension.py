acfc5d0040b338d16bb9fc8e716a05f563b5583c
Collins Kibet <kollcibe05@gmail.com>
2025-11-13 08:11:43 +0300

    SOLVED

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
-
+# my_list = [1,2,3,4,5,6]
+# str_list = ["one", "Two","Three"]
 def return_evens(num_list):
+    # even = (n%2 == 0) == True
+    return([n for n in num_list if (n % 2 == 0) == True])
+    # return([n for n in num_list if n == even])
+
     pass
+
 
 def make_exclamation(sentence_list):
+    return([f"{n}!" for n in sentence_list])
     pass
+
+# print(return_evens(my_list))
+# print(make_exclamation(str_list))
